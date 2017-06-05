6a21c73cd70c4ba037037129a3e645f8d63c5969
ViktorDulyak <vitiaduleac@gmail.com>
2017-06-06 00:58:46 +0300

    Add files via upload

---
+++ zalik.github.io/banners.js
@@ -0,0 +1,8 @@
+ if (navigator.userAgent.indexOf("Android") >= 1)
+ document.write("<a href='https://ad.admitad.com/g/34y5hcjxsx2ebb54518f0aa7800eed/?subid=wd' target='_blank' rel='nofollow'><img src='../bnr/ybb1.jpg' alt='img' class='banner'/></a>");
+ else if (navigator.userAgent.indexOf("iPhone") >= 1)
+ document.write("<a href='https://ad.admitad.com/g/yj7kyxk12a2ebb54518fa3a38c19f2/?subid=wd' target='_blank' rel='nofollow'><img src='../bnr/yyb2.jpg' alt='img' class='banner'/></a>");
+ else if (navigator.userAgent.indexOf("iPad") >= 1)
+ document.write("<a href='https://ad.admitad.com/g/yj7kyxk12a2ebb54518fa3a38c19f2/?subid=wd' target='_blank' rel='nofollow'><img src='../bnr/yyb3.jpg' alt='img' class='banner'/></a>");
+ else
+ document.write("<a href='http://nfemo.com/click-CQDVWCK0-NLJQCDUE?bt=25&tl=1&sa=wd' target='_blank' rel='nofollow'><img src='../bnr/vp2.jpg' alt='img' class='banner'/></a>");
